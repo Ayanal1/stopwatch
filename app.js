c488f2ceb2bd208dbb6e1362229ca79bdace3e63
AYAN ALI <73759379+Ayanal1@users.noreply.github.com>
2021-09-22 02:21:46 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 minute = document.getElementById("minute").innerText
 second = document.getElementById("second").innerText
 milisecond = document.getElementById("milisecond").innerText
@@ -9,13 +8,14 @@ var intervalfirst;
 
 function start() {
     intervalfirst = setInterval(abc, 100);
-    document.getElementById("start").setAttribute("disabled",true)
+    document.getElementById("start").setAttribute("disabled", true)
     document.getElementById("reset").removeAttribute("disabled")
     document.getElementById("pause").removeAttribute("disabled")
+    document.getElementById("split").removeAttribute("disabled")
 }
 
 function abc() {
-    
+
     mil++
     document.getElementById("milisecond").innerText = mil;
     if (mil > 9) {
@@ -27,44 +27,51 @@ function abc() {
         sec = 0
         min++
         document.getElementById("minute").innerText = min;
-        
+
     }
 }
 
-function reset(){
+function reset() {
     clearInterval(intervalfirst)
     min = 0
     sec = 0
     mil = 0
     minute = document.getElementById("minute").innerText = min
-    second = document.getElementById("second").innerText= sec
-    milisecond = document.getElementById("milisecond").innerText= mil
+    second = document.getElementById("second").innerText = sec
+    milisecond = document.getElementById("milisecond").innerText = mil
     document.getElementById("start").removeAttribute("disabled")
-    document.getElementById("reset").setAttribute("disabled",true)
-    document.getElementById("pause").setAttribute("disabled",true)
-
+    document.getElementById("reset").setAttribute("disabled", true)
+    document.getElementById("pause").setAttribute("disabled", true)
+    l1 = document.getElementById("lap").innerHTML = null
+    // location.reload()
 
 }
-function pause(){
+function pause() {
     clearInterval(intervalfirst)
     // min = 0
     // sec = 0
     // mil = 0
     minute = document.getElementById("minute").innerText = min
-    second = document.getElementById("second").innerText= sec
-    milisecond = document.getElementById("milisecond").innerText= mil
+    second = document.getElementById("second").innerText = sec
+    milisecond = document.getElementById("milisecond").innerText = mil
     document.getElementById("start").removeAttribute("disabled")
-    document.getElementById("pause").setAttribute("disabled",true)
-    
-    l1=  document.getElementById("lap")
-    l2=  document.createElement("h6")
-    l3=  document.createTextNode(minute+" :mins ,"+ second+" :sec ,"+ milisecond+" :mili-sec ,")
+    document.getElementById("pause").setAttribute("disabled", true)
+    document.getElementById("split").setAttribute("disabled", true)
+}
+var i = 0;
+function split() {
+    i++
+    minute = document.getElementById("minute").innerText = min
+    second = document.getElementById("second").innerText = sec
+    milisecond = document.getElementById("milisecond").innerText = mil
+    l1 = document.getElementById("lap")
+    l2 = document.createElement("h6")
+    hr = document.createElement("hr")
+
+    l3 = document.createTextNode(i + "#  " + minute + "min, " + second + "sec, " + milisecond + "ms")
     l2.appendChild(l3);
     l1.appendChild(l2);
-    
-
-    
-    
+    l2.appendChild(hr);
 
 }
 
